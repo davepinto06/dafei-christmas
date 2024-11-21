a6aee92162e843e9eb21d54e76810dc5fe3d0f28
Dave Pinto <estradapintodavid@gmail.com>
2024-11-20 22:17:40 -0600

    Add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  output: 'server',
+  adapter: cloudflare()
 });
